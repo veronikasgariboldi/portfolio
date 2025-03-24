eb49d11717c6ea80a02a4ad0620655804efae124
veronikasgariboldi <verca.ribol@gmail.com>
2025-03-24 22:43:12 +0100

    overview layout + animation

--- src/components/projects-overview.js
+++ src/components/projects-overview.js
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
 import './projects-overview.css';
 import Projectlayout1 from './projects shortview/project1';
 import Projectlayout2 from './projects shortview/project2';
@@ -6,9 +6,26 @@ import Projectlayout2 from './projects shortview/project2';
 function Overview() {
 
     const projectData = [
-        { layout: Projectlayout1, imageUrl: "/path/to/image1.jpg", title: "Egg Timer", date: "2025", description: "A simple timer application for perfect eggs every time." },
-        { layout: Projectlayout2, imageUrl: "/path/to/image2.jpg", title: "Weather App", date: "2024", description: "Real-time weather forecasting application." },
-        { layout: Projectlayout1, imageUrl: "/path/to/image2.jpg", title: "Weather App", date: "2024", description: "Real-time weather forecasting application." },
+        { 
+            layout: Projectlayout1, 
+            imageUrl: "/path/to/image1.jpg", 
+            title: "Egg Timer", date: "March 2025", 
+            description: "A simple and cute timer application for perfect eggs every time. Inspired by the tiktoker @nashla" 
+        },
+
+        { 
+            layout: Projectlayout2, 
+            imageUrl: "/path/to/image2.jpg", 
+            title: "Sales Webpage", date: "February 2024", 
+            description: "A commersial webpage, able to sell corcheted products.\n infostun.de" 
+        },
+        
+        { 
+            layout: Projectlayout1, 
+            imageUrl: "/path/to/image2.jpg", 
+            title: "Weather App", date: "2024", 
+            description: "Real-time weather forecasting application." 
+        },
 
     ];
     
@@ -33,12 +50,13 @@ function Overview() {
                         }}
                         viewport={{ once: true, amount: 1 }} // Trigger animation when fully in view
                     >
-                        <Layout
-                            imageUrl={project.imageUrl}
-                            title={project.title}
-                            date={project.date}
-                            description={project.description}
-                        />
+                         {/* Wrapping the project content in Link */}
+                                <Layout
+                                    imageUrl={project.imageUrl}
+                                    title={project.title}
+                                    date={project.date}
+                                    description={project.description}
+                                />
                     </motion.div>
                 );
             })}
