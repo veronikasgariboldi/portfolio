b788901d91c986e15e9d2c04c6a0a653640f79b0
veronikasgariboldi <verca.ribol@gmail.com>
2025-03-29 00:42:50 +0100

    about.js animation

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,24 +1,16 @@
-import './home.css';
-
-
-function Home() { 
-    return (
-        <div className='Homepage'>
-            <div className="MainTitle">
-                <div className="headline-container">
-                    <div className="headline">
-                        <h1 className="first-row"><span className="typing-text"> <span className="Yellow">About Veronika Sgariboldi</span></span></h1>
-                        <p className="in-between-row"><span className="typing-text"> <span className="gray">student, front-end developer, and much more!</span></span></p>
-                        <h1 className="third-row"><span className="typing-text">the one <span className="green">you</span> need</span></h1>
-                        <div className="arrow-container">
-                            <div className="arrow"></div>
-                        </div>  
-                        <p className='scroll'>you can scroll down now</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    ); 
+import ScrollVelocity from '../components/scrollthing';
+import './about.css'; 
+function About() {
+  return (
+    <div className="about-container"> 
+      <ScrollVelocity 
+        texts={['so talented!', 'so true']} 
+        velocity={50} 
+        numCopies={50}
+        className="custom-scroll-text"
+      />
+    </div>
+  );
 }
 
-export default Home;
+export default About;
