31f3eeca8bb5d05be9726ad5198735e99dc6c161
veronikasgariboldi <verca.ribol@gmail.com>
2025-03-29 00:50:55 +0100

    about.js the question

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,13 +3,21 @@ import './about.css';
 function About() {
   return (
     <div className="about-container"> 
-      <ScrollVelocity 
-        texts={['so talented!', 'so true']} 
-        velocity={50} 
-        numCopies={50}
-        className="custom-scroll-text"
-      />
-    </div>
+        <div className='intro'>
+            <div className='title'>
+            <h1 className="asking-the-question">WHO are you??</h1>
+
+            </div>
+        </div>
+            <div className='scrolling'>
+                <ScrollVelocity 
+                    texts={['so talented!', 'so true']} 
+                    velocity={50} 
+                    numCopies={50}
+                    className="custom-scroll-text"
+                />
+            </div>
+        </div>
   );
 }
 
